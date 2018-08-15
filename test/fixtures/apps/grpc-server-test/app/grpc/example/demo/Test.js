cb1fcc44c8de3a21717ed3542917a8bdada9bf8b
j0 <tw949561391@hotmail.com>
2018-08-15 17:09:46 +0800

    1.5.8

--- test/fixtures/apps/grpc-server-test/app/grpc/example/demo/Test.js
+++ test/fixtures/apps/grpc-server-test/app/grpc/example/demo/Test.js
@@ -2,17 +2,9 @@
 const BaseRpc = require('../../../../../../../../index').BaseGrpc;
 
 module.exports = class Test extends BaseRpc {
-
-
-  echo(call, callback) {
-    this.app.logger.debug('this is a test rpc');
-    const metadata = new this.grpc.Metadata();
-    metadata.set('from', 'server');
-    call.sendMetadata(metadata);
-    // send result
-
-    callback(null, {
-      msg: `grpc test success from ${call.request.name}`,
-    });
-  }
+    async Echo() {
+        return {
+            msg: `grpc test success `,
+        }
+    }
 };
